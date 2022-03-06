9e131203bc6520ec00e4a5e55055561a7e2c2c17
dream-huan <52626649+dream-huan@users.noreply.github.com>
2022-03-06 09:14:05 +0000

    Enter

--- bitmap/text.cpp
+++ bitmap/text.cpp
@@ -1,41 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Tries{
+class Solution {
 public:
-    void insertStr(string s){
-        Tries* node=this;
-        for(int i=0;i<s.length();++i){
-            if(!node->children[s[i]-'a']) node->children[s[i]-'a']=new Tries();
-            node=node->children[s[i]-'a'];
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int>ans;
+        deque<int>q;
+        int a1,a2,t,g;
+        for(int i=0;i<nums.size();++i) q.emplace_back(nums[i]);
+        while(!q.empty()){
+            a1=q[0];
+            q.pop_front();
+            a2=q[0];
+            g=__gcd(a1,a2);
+            if(g>1){
+                t=1ll*a1*a2/g;
+                q.pop_front();
+                q.emplace_front(t);
+            }else{
+                ans.emplace_back(a1);
+            }
         }
-        node->isEnd=true;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
-    bool checkPrefix(string s){
-        Tries* node=this;
-        for(int i=0;i<s.length();++i){
-            node=node->children[s[i]-'a'];
-            return node!=nullptr;
-        }
-        return node!=nullptr;
-    }
-    bool checkStr(string s){
-        Tries* node=this;
-        for(int i=0;i<s.length();++i){
-            if(node!=nullptr) node=node->children[s[i]-'a'];
-        }
-        return node!=nullptr&&node->isEnd;
-    }
-private:
-    vector<Tries*>children=vector<Tries*>(26);
-    bool isEnd=false;
 };
 
 int main(){
-    Tries* tree=new Tries();
-    tree->insertStr("abewferrg");
-    cout<<tree->checkPrefix("abe")<<"\n";
-    cout<<tree->checkStr("ade")<<"\n";
-    cout<<tree->checkStr("abewferrg")<<"\n";
+    Solution *a=new Solution();
+    int n;
+    vector<int>v(n);
+    cin>>n;
+    for(int i=0;i<n;++i) cin>>v[i];
+    vector<int>ans;
+    ans=a->replaceNonCoprimes(v);
+    for(int i=0;i<ans.size();++i) cout<<ans[i]<<" "; 
     return 0;
 }
